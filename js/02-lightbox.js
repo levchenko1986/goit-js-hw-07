3bc04fe7f7b6c9d96254cd93dd0097a9dcc6eaec
levchenko1986 <89630449+levchenko1986@users.noreply.github.com>
2021-11-10 17:06:52 +0200

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,11 +6,9 @@ console.log(galleryItems);
 const galleryListEl = document.querySelector('.gallery');
 
 const makeItemCard = function ({preview, original, description} = {}) {
-    return`
-<a class="gallery__item" href="${original}">
-    <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>
-`;
+    return`<li><a class="gallery__item" href=${original}>
+    <img class="gallery__image" src=${preview} alt=${description} />
+</a></li>`;
 };
 
 const galleryItemElements = galleryItems.map(makeItemCard).join('');
